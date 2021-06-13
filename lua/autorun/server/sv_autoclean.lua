7350f6f14072caddc5c134f16553169209e9d612
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-06-13 03:33:10 -0700

    Refactor to use shared messages

--- lua/autorun/server/sv_autoclean.lua
+++ lua/autorun/server/sv_autoclean.lua
@@ -1,52 +1,11 @@
 util.AddNetworkString( "CFC_RunAutoClean" )
+local rawget = rawget
+local IsValid = IsValid
 
+local messageCount = #CFCAutoClean.clearingServerMessages
 local ConVarFlags = {FCVAR_ARCHIVE, FCVAR_NOTIFY}
 local DEFAULT_CLEAN_INTERVAL_IN_SECONDS = "500"
-CreateConVar( "cfc_autoclean", DEFAULT_CLEAN_INTERVAL_IN_SECONDS, ConVarFlags, "Autocleans the server based on seconds given" )
-
-local clearingServerMessages = {
-    "[CFC - AutoClean] Scrubbing the ethernet...",
-    "[CFC - AutoClean] Dusting the heatsinks...",
-    "[CFC - AutoClean] Applying more thermal paste...",
-    "[CFC - AutoClean] Tidying...",
-    "[CFC - AutoClean] Replacing internet juice...",
-    "[CFC - AutoClean] Washing threads...",
-    "[CFC - AutoClean] Cleaning forks...",
-    "[CFC - AutoClean] Downloading more RAM...",
-    "[CFC - AutoClean] Updating drivers...",
-    "[CFC - AutoClean] Installing more antivirus...",
-    "[CFC - AutoClean] Brushing teeth...",
-    "[CFC - AutoClean] Cleaning behind the ears...",
-    "[CFC - AutoClean] Vacuuming...",
-    "[CFC - AutoClean] Taking vitamins...",
-    "[CFC - Antibullying] Calling moms...",
-    "[CFC - Antiminge] Removing users...",
-    "[CFC - Criminal] Stealing copies of phatmania...",
-    "[CFC - Raincore] Soaking your builds...",
-    "[CFC - Minecraftcore] Removing legokidlogan's minecraft E2...",
-    "[CFC - Anticheat] Removing RadioJackal...",
-    "[CFC - Antispam] Removing chat...",
-    "[CFC - Antimicspam] Removing voicechat...",
-    "[CFC - Robostaff] Banning all humans...",
-    "[CFC - Spartan] Deleting boats...",
-    "[CFC - Minecraft] Commencing crash...",
-    "[CFC - Phatcore] Removing minges...",
-    "[CFC - Antiphatcore] Commencing dazzling...",
-    "[CFC - Awards] Handing MetaKnight award for having lemon cat...",
-    "[CFC - Piracy] Downloading pirated phatmania...",
-    "[CFC - Antirdm] Calling admins...",
-    "[CFC - Musiccore] Purchasing phatmania album...",
-    "[CFC - Fashion] Purchasing phatso merchandise...",
-    "[CFC - Mingecore] Spawning emitters...",
-    "[CFC - Chefcore] Burning the food...",
-    "[CFC - Chefcore] Consulting Gordon Ramsay...",
-    "[CFC - Discordmanager] Removing permissions...",
-    "[CFC - Fallcore] Did you know you can reduce the amount of damage you take from falling by crouching and looking down before you impact a surface?"
-}
-
-local function getClearingServerMessage()
-    return table.Random( clearingServerMessages )
-end
+local autocleanInterval = CreateConVar( "cfc_autoclean", DEFAULT_CLEAN_INTERVAL_IN_SECONDS, ConVarFlags, "Autocleans the server based on seconds given" )
 
 local function runCleanupCommandsOnPlayers()
     local notificationMsg = getClearingServerMessage()
@@ -58,8 +17,12 @@ end
 
 local function removeUnownedWeapons()
     local removedCount = 0
+    local allEnts = ents.GetAll()
+    local entsCount = #allEnts
 
-    for _, entity in pairs( ents.GetAll() ) do
+    for i = 1, entsCount do
+        local ent = rawget( allEnts, i )
+
         if IsValid( entity ) then
             local isUnownedWeapon = entity:IsWeapon() and not IsValid( entity.Owner )
 
@@ -84,7 +47,4 @@ local function runCleanupFunctions()
     removeUnownedWeapons()
 end
 
-timer.Create( "CFC_AutoClean", GetConVar( "cfc_autoclean" ):GetInt(), 0, runCleanupFunctions )
-
-hook.Remove( "APG_lagDetected", "CFC_CleanOnLag" )
-hook.Add( "APG_lagDetected", "CFC_CleanOnLag", runCleanupFunctions )
+timer.Create( "CFC_AutoClean", autocleanInterval:GetInt(), 0, runCleanupFunctions )
--- lua/autorun/sh_autoclean.lua
+++ lua/autorun/sh_autoclean.lua
@@ -1,15 +1,41 @@
---[[
--- TODO: Find a way to get this type of structure to work
--- Ideally we'd have one place that both the client and server can read the clientside commands
+CFCAutoClean = {}
 
-CFC = CFC or {}
-
-local AutoClean = {}
-
-local ClientCommands = {}
-ClientCommands["r_cleardecals"] = true
-ClientCommands["stopsound"] = true
-
-AutoClean.ClientCommands = ClientCommands
-CFC.AutoClean = AutoClean
---]]
+CFCAutoClean.clearingServerMessages = {
+    "[CFC - AutoClean] Scrubbing the ethernet...",
+    "[CFC - AutoClean] Dusting the heatsinks...",
+    "[CFC - AutoClean] Applying more thermal paste...",
+    "[CFC - AutoClean] Tidying...",
+    "[CFC - AutoClean] Replacing internet juice...",
+    "[CFC - AutoClean] Washing threads...",
+    "[CFC - AutoClean] Cleaning forks...",
+    "[CFC - AutoClean] Downloading more RAM...",
+    "[CFC - AutoClean] Updating drivers...",
+    "[CFC - AutoClean] Installing more antivirus...",
+    "[CFC - AutoClean] Brushing teeth...",
+    "[CFC - AutoClean] Cleaning behind the ears...",
+    "[CFC - AutoClean] Vacuuming...",
+    "[CFC - AutoClean] Taking vitamins...",
+    "[CFC - Antibullying] Calling moms...",
+    "[CFC - Antiminge] Removing users...",
+    "[CFC - Criminal] Stealing copies of phatmania...",
+    "[CFC - Raincore] Soaking your builds...",
+    "[CFC - Minecraftcore] Removing legokidlogan's minecraft E2...",
+    "[CFC - Anticheat] Removing RadioJackal...",
+    "[CFC - Antispam] Removing chat...",
+    "[CFC - Antimicspam] Removing voicechat...",
+    "[CFC - Robostaff] Banning all humans...",
+    "[CFC - Spartan] Deleting boats...",
+    "[CFC - Minecraft] Commencing crash...",
+    "[CFC - Phatcore] Removing minges...",
+    "[CFC - Antiphatcore] Commencing dazzling...",
+    "[CFC - Awards] Handing MetaKnight award for having lemon cat...",
+    "[CFC - Piracy] Downloading pirated phatmania...",
+    "[CFC - Antirdm] Calling admins...",
+    "[CFC - Musiccore] Purchasing phatmania album...",
+    "[CFC - Fashion] Purchasing phatso merchandise...",
+    "[CFC - Mingecore] Spawning emitters...",
+    "[CFC - Chefcore] Burning the food...",
+    "[CFC - Chefcore] Consulting Gordon Ramsay...",
+    "[CFC - Discordmanager] Removing permissions...",
+    "[CFC - Fallcore] Did you know you can reduce the amount of damage you take from falling by crouching and looking down before you impact a surface?"
+}
