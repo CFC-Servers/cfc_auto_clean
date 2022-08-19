80b5ead872334a72cdf6caf23bd4b02a81d803e6
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-08-19 15:01:04 -0700

    Add cvar callback to update timer

--- lua/autorun/server/sv_autoclean.lua
+++ lua/autorun/server/sv_autoclean.lua
@@ -2,12 +2,17 @@ util.AddNetworkString( "CFC_RunAutoClean" )
 
 local IsValid = IsValid
 local random = math.random
+local rawget = rawget
 
 local ConVarFlags = { FCVAR_ARCHIVE }
 local autocleanEnabled = CreateConVar( "cfc_autoclean_enabled", 1, ConVarFlags, "Enables autoclean on the server." )
 local autocleanInterval = CreateConVar( "cfc_autoclean_interval", 500, ConVarFlags, "Interval (in seconds) when the server will run autoclean commands." )
 local autocleanClientNotify = CreateConVar( "cfc_autoclean_notification_enabled", 1, ConVarFlags, "Enables the client-side notification autoclean uses." )
 local autocleanPrefix = CreateConVar( "cfc_autoclean_prefix", "CFC Auto Clean", ConVarFlags, "The prefix autoclean uses when printing messages to clients." )
+
+cvars.AddChangeCallback( "cfc_autoclean_interval", function( _, _, value )
+    timer.Adjust( "CFC_AutoClean", tonumber( value ) )
+end )
 
 local messagesCount = #CFCAutoClean.clearingServerMessages
 
@@ -22,8 +27,11 @@ end
 local function removeUnownedWeapons()
     local removedCount = 0
     local allEnts = ents.GetAll()
+    local entsCount = #allEnts
 
-    for _, ent in ipairs( allEnts ) do
+    for i = 1, entsCount do
+        local ent = rawget( allEnts, i )
+
         if IsValid( ent ) then
             local isUnownedWeapon = ent:IsWeapon() and not IsValid( ent.Owner )
 
